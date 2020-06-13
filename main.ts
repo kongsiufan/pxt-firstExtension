70ef71117f6723978d42ed0b744afe3208f74fa4
kongsiufan <66825829+kongsiufan@users.noreply.github.com>
2020-06-13 09:51:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+function displaySomething () {
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Heart)
+}
+input.onButtonPressed(Button.A, function () {
+    displaySomething()
+})
+basic.forever(function () {
+	
+})
